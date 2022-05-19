f08baabbc2f3acef519e31b74ccfa81375b07dcc
RitsukiShuto <rtk410s@gmail.com>
2022-05-19 14:47:09 +0900

    人工データ生成部を実装

--- semi/DNN.py
+++ semi/DNN.py
@@ -4,6 +4,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+# 人工データを作成
 N1 = 100
 N2 = 100
 N3 = 100
@@ -25,7 +26,18 @@ X2[:, 1] = np.random.normal(20, 5, N2)
 X3[:, 0] = np.random.normal(30, 6, N3)
 X3[:, 1] = np.random.normal(90, 4, N3)
 
+X = np.concatenate([X1, X2, X3], 0)
+Y = np.concatenate([Y1, Y2, Y3], 0)
+
+print(X.shape)
+print(Y.shape)
+
 # 描画
 fig = plt.figure(figsize = (8, 8))
 plt.scatter(X1[:, 0], X1[:, 1], marker='o', c = 'red', s=20)
-plt.show()
+plt.scatter(X2[:, 0], X2[:, 1], marker='o', c = 'blue', s=20)
+plt.scatter(X3[:, 0], X3[:, 1], marker='o', c = 'green', s=20)
+
+# plt.show()
+
+# 
